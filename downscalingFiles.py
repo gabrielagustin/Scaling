0a4256da7af2ed63749f0522b34dac87240ad45e
gabrielagustin <gabiagus@gmail.com>
2019-06-20 13:38:42 -0300

    cleaning code

--- downscalingFiles.py
+++ downscalingFiles.py
@@ -3,7 +3,14 @@
 
 """
 Created on Sun Jun 04 19:41:04 2017
+
 @author: gag 
+
+Script that allows apply the operation of Downscaling to a set of images, indicating:
+ - n steps of downscaling
+ - type and order of the filter to be used
+
+Type filter: "db": Daubechies, "Symlets", "Haar", "Coiflets", "CDF", "Morlet"
 
 """
 
@@ -44,15 +51,27 @@ fechaModis.append("2016-03-21")
 #ficheros = os.listdir(path)
 ##print len(ficheros)
 #for i in range(0, len(ficheros)):
+
 for i in range(0, len(fechaModis)):
     #print i
     #print "Archivo: " + str(ficheros[i])
     #downscaling.applyDownscalingN(3,"Coiflets", 4, str(path +ficheros[i] + '/' ), nameFile)
     path = "/.../MODIS/"+fechaModis[i]+"/"
     nameFile = "NDVI_reproyectado_recortado"
-    #### parametros nTimes, typeFilter, orderFilter, path, nameFileIn
-    #### orderFilter "db": Daubechies, "Symlets", "Haar", "Coiflets", "CDF", "Morlet"
+
+    # parametros nTimes, typeFilter, orderFilter, path, nameFileIn, orderFilter
+    # Type filter:  "db": Daubechies, "Symlets", "Haar", "Coiflets", "CDF", "Morlet"
+
     #downscaling.applyDownscalingN(3,"Coiflets", 4, path, nameFile)
     #downscaling.applyDownscalingN(3,"Symlets", 4, path, nameFile)
     #downscaling.applyDownscalingN(3,"db", 4, path, nameFile)
     downscaling.applyDownscalingN(3,"Haar", 4, path, nameFile)
+
+
+
+
+
+
+
+
+
